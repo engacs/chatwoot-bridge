7a45fa0c27c76314cbc176bf375df362c10f718d
Eng Acs <me@engacs.pw>
2026-06-08 18:39:11 +0300

    Update build.ts

--- script/build.ts
+++ script/build.ts
@@ -48,6 +48,9 @@ async function buildAll() {
     packages: "external",
     tsconfig: "tsconfig.json",
     logLevel: "info",
+    banner: {
+      js: `import { fileURLToPath } from 'url'; import { dirname } from 'path'; const __filename = fileURLToPath(import.meta.url); const __dirname = dirname(__filename);`,
+    },
   });
 }
 
